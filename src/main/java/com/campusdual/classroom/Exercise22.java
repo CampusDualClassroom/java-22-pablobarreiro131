a3c2ad099aa92ddae39875eeacd18992ef0cc10a
Pablo <pablobarreiro131@gmail.com>
2025-05-23 13:03:07 +0200

    Ejercicio 22, completado y testeado.

--- src/main/java/com/campusdual/classroom/Exercise22.java
+++ src/main/java/com/campusdual/classroom/Exercise22.java
@@ -7,19 +7,38 @@ import java.util.Set;
 public class Exercise22 {
 
     public static List<String> createArrayList() {
-
+        List<String> lista = new ArrayList<>();
+        lista.add("GKFFD");
+        lista.add("TNANA");
+        lista.add("MPMSL");
+        lista.add("PSWME");
+        lista.add("LZMLF");
+        lista.add("JYEBV");
+        lista.add("YELNT");
+        lista.add("JSNKR");
+        lista.add("JFESF");
+        lista.add("TMJLL");
+        return lista;
     }
 
     public static void printElementsAndIndex(List<String> customList) {
-
+        int cont = 1;
+        for(String s: customList){
+            System.out.println(cont + ": " + s);
+            cont++;
+        }
     }
 
     public static boolean addElementToList(List<String> customList, String element) {
-
+        return customList.add(element);
     }
 
     public static void main(String[] args) {
-
+        List<String> lista = createArrayList();
+        if(addElementToList(lista, "AAAAA")){
+            System.out.println("Se añade.");
+        } else System.out.println("No se añade.");
+        printElementsAndIndex(lista);
     }
 
 
